dfd97e20443d72039bd9b15f4aa44c3a5df1ad44
Anton <79325492+anton001-web@users.noreply.github.com>
2022-07-08 21:12:38 +0300

    modal gallery (section2) - done

--- src/components/Section2.js
+++ src/components/Section2.js
@@ -4,36 +4,58 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import {Navigation} from "swiper";
 
 import 'swiper/scss'
-import {act} from "react-dom/test-utils";
 
 
 const Section2 = () => {
+    const [slidesView, setSlidesView] = useState(3 )
     const [activeSlide, setActiveSlide] = useState(0);
+    const galleryBg = React.createRef()
+
+    const handleGallery = (e) => {
+        if(e.target.dataset.openga) {
+            if(!galleryBg.current.classList.contains('swiper-gallery')) {
+                setSlidesView(1)
+                console.log(setSlidesView)
+                galleryBg.current.classList.add('swiper-gallery')
+            } else {
+                if(e.target.dataset.closega) {
+                    galleryBg.current.classList.remove('swiper-gallery')
+                    setSlidesView(3)
+                }
+            }
+        }
+    }
+
+    const slidesPerView = () => {
+        return slidesView
+    }
 
     return (
         <section className='firstSwiper-section'>
-            <Swiper
-                modules={[Navigation]}
-                slidesPerView={1}
-                centeredSlides={true}
-                centeredSlidesBounds={true}
-                navigation
-                loop={true}
-                spaceBetween={-65}
-                breakpoints={{
-                    767: {
-                        slidesPerView: 3
+            <div className='gallery-wrap' ref={galleryBg} data-openga data-closega onClick={handleGallery}>
+                <Swiper
+                    modules={[Navigation]}
+                    slidesPerView={slidesPerView()}
+                    centeredSlides={true}
+                    centeredSlidesBounds={true}
+                    navigation
+                    loop={true}
+                    spaceBetween={-65}
+                    breakpoints={{
+                        767: {
+                            slidesPerView: slidesPerView()
+                        }
+                    }}
+                >
+                    {
+                        section1SliderData.map((item, index) => (
+                            <SwiperSlide key={index} >
+                                <img src={item.img} onClick={() => handleGallery} data-openga/>
+                            </SwiperSlide>
+                        ))
                     }
-                }}
-            >
-                {
-                    section1SliderData.map((item, index) => (
-                        <SwiperSlide key={index}>
-                            <img src={item.img}/>
-                        </SwiperSlide>
-                    ))
-                }
-            </Swiper>
+                </Swiper>
+            </div>
         </section>
     )
 }
