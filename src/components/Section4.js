439015666375f1bd3b89c3be78e6e21258dd015a
Anton <79325492+anton001-web@users.noreply.github.com>
2022-07-10 14:54:32 +0300

    section4 (gallery section) - done

---
+++ src/components/Section4.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {section4GalleryImages} from "../utils/section4-galleryImages";
+
+
+const Section4 = () => {
+    return (
+        <section className='section4-gallery'>
+            <div className='container'>
+                <div className='section4-gallery__content'>
+                    <h1 className='section4-gallery__title'>Виды услуг</h1>
+                    <div className='section4-grid__block'>
+                        {
+                            section4GalleryImages.map((item, index) => (
+                                <div className='grid_item' data-grid={item.item}>
+                                    <img src={item.img} alt="" />
+                                    <p className='grid_item--title'>{item.title}</p>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Section4
---
+++ src/utils/section4-galleryImages.js
@@ -0,0 +1,13 @@
+export const section4GalleryImages = [
+    {img: './assets/images/image804.png', item: 'one', title: 'Семейная фотосессия дети(+14)'},
+    {img: './assets/images/image803.png', item: 'two', title: 'Фотосессия для парней'},
+    {img: './assets/images/image807.png', item: 'three', title: 'Фотосессия Лукбук'},
+    {img: './assets/images/image810.png', item: 'four', title: 'Групповая фотосессия'},
+    {img: './assets/images/image811.png', item: 'five', title: 'Фотосессия для беременных'},
+    {img: './assets/images/image813.png', item: 'six', title: 'Фотосессия для девушек'},
+    {img: './assets/images/image817.png', item: 'seven', title: 'Фотосессия с животными'},
+    {img: './assets/images/image821.png', item: 'eleven', title: 'Фотосессия "деловой стиль" '},
+    {img: './assets/images/image826.png', item: 'eight', title: 'Парная фотосессия'},
+    {img: './assets/images/image824.png', item: 'nine', title: 'Фотосессия для подростков'},
+    {img: './assets/images/image814.png', item: 'ten', title: 'Фотосессия Лав Стори'},
+]
